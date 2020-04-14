a2562e16c28f7c41f62bab2194b5c0b04c3c3818
Carto-NA <63336882+Carto-NA@users.noreply.github.com>
2020-04-14 12:20:57 +0200

    Initialisation du fichier SQL pour les droits

---
+++ sql/plan_urgence_99_droit.sql
@@ -0,0 +1,8 @@
+/* PLAN_URGENCE V1.0 */
+/* Creation des droits sur l'ensemble des objets */
+/* plan_urgence_99_droit.sql */
+/* PostgreSQL/PostGIS */
+/* Conseil régional Nouvelle-Aquitaine - https://cartographie.nouvelle-aquitaine.fr/ */
+/* Auteur : Tony VINCENT */
+
+
