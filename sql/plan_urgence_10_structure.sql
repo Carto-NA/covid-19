8e237d165e3debe900f2830863fe2670efc4c7a5
Carto-NA <63336882+Carto-NA@users.noreply.github.com>
2020-04-14 12:19:55 +0200

    Initialisation du fichier SQL pour la structure

---
+++ sql/plan_urgence_10_structure.sql
@@ -0,0 +1,9 @@
+/* PLAN_URGENCE V1.0 */
+/* Creation de la structure des données (schéma, tables, séquences, triggers,...) */
+/* plan_urgence_10_structure.sql */
+/* PostgreSQL/PostGIS */
+/* Conseil régional Nouvelle-Aquitaine - https://cartographie.nouvelle-aquitaine.fr/ */
+/* Auteur : Tony VINCENT */
+
+
+
